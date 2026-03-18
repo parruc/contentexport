aad474ce9ee11ded78b64ca014bbbc540e8393a6
Matteo Parrucci <m.parrucci@unibo.it>
2026-03-18 16:27:46 +0100

    added last modifier export

--- contentexport/export_content.py
+++ contentexport/export_content.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 from collective.exportimport.export_content import ExportContent
+from unibo.api.content import last_modifier
 
 import logging
 
@@ -87,4 +88,7 @@ class CustomExportContent(ExportContent):
         """Used this to modify the serialized data.
         Return None if you want to skip this particular object.
         """
+        modifier_id = last_modifier(obj)
+        if modifier_id:
+            item["last_modifier"] = modifier_id
         return item
