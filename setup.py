921ab92ec4e00e0331d5fbbd7069bc480879ab0e
Matteo Parrucci <m.parrucci@unibo.it>
2026-07-14 15:56:03 +0200

    Preparing release 0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='contentexport',
-    version='0.3.dev0',
+    version='0.3',
     description='Example for extending collective.exportimport',
     url='https://github.com/starzel/contentexport',
     author='Philip Bauer',
